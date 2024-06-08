9db940cb901b86c8f6fdf5cb2f317abc7fd2fbde
Damian Mostert <damianmostert86@gmail.com>
2024-06-08 10:22:31 +0200

    update server

--- src/lib/engine/lib/use-socket.js
+++ src/lib/engine/lib/use-socket.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
-const socket = io("http://localhost:4000");
+const socket = io("node-js-game-server-six.vercel.app");
 
 export default function useSocket(states = []){
 	const [data,setData] = useState({});
