42fdaf1531e8a8ddb3d67d79fa46d15282a25199
damian <damian@fusebox.co.za>
2024-08-15 13:54:53 +0200

    update home.js

--- src/lib/resources/levels/Home.js
+++ src/lib/resources/levels/Home.js
@@ -10,19 +10,6 @@ const boundaries = [
     passThrough:true,
 
   },
-  /* {
-    bot:true,
-    character:"Aether",
-    top:0,
-    left:1,
-    action:(input)=>{
-      if(input.inRange){
-        input.character.left= input.left;
-        input.character.action = "attack"
-      }
-      return input
-    }
-  }, */
   {
       top: 150,
       left: -450,
@@ -42,54 +29,45 @@ const boundaries = [
     },
     {
       top: 41,
-      left: 1,
-      width: 4,
+      left:0,
+      width: 1,
       height: 1,
-      texture: "Dirt",
       id:"Message",
-      inRangeBlocks:()=>({
-        range:250,
-        action:(boundary)=>{
-          window.gameDom.updateBigMessage("Hi there");
-          boundary.message = "Checkpoint"
-          return boundary; 
-        }}),
-      outRangeBlocks:()=>({
-        range:250,
-        action:(boundary)=>{
-          window.gameDom.updateBigMessage(null);
-          boundary.message = null
-          return boundary;  
-        }}),
+      id:"Floor",
+      inRange(boundary){
+        window.gameDom.updateBigMessage("Hi there");
+        boundary.message = "start"
+        return boundary; 
+      },
+      outRange(boundary){
+        window.gameDom.updateBigMessage(null);
+        boundary.message = null
+        return boundary;  
+      },
 
       
     },
     {
       top: 50,
-      left: 10,
+      left: 9,
       width: 1,
       height: 1,
       texture: "Dirt",
       id:"Message2",
       inRangeBlocks:()=>({
-        range:250,
+        range:150,
         action:(boundary)=>{
           window.gameDom.updateBigMessage("Welcome to the game");
           boundary.texture = "Grass";
           return boundary; 
         }}),
       outRangeBlocks:()=>({
-        range:250,
+        range:150,
         action:(boundary)=>{
-          window.gameDom.updateBigMessage(null);
           boundary.texture = "Dirt";
-          boundary.message = null
           return boundary;  
         }}),
-
-      
     },
-
     ...createSlope(80, 0, 40, 40, false, "vertical", "Grass"),
     ...createQuarterCircle(120, -40, 40, false, "horizontal", "Dirt"),
     ...createQuarterCircle(120, -40, 40, true, "vertical", "Dirt"),
